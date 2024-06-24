16930670e80618c3ddc6fca1267a7b7e2141fad2
MarianeCamilo <marianecamilo03@gmail.comgit config --global user.email>
2024-06-24 19:18:02 -0300

    att

--- src/screens/Hard_To_Imagine.jsx
+++ src/screens/Hard_To_Imagine.jsx
@@ -8,13 +8,12 @@ const songs = [
   { id: "23", title: "Compass", album: "Hard To Imagine", year: 2018, url: "https://youtu.be/j56dEcq7ryo?si=Bu9uBdLHU84vus31" },
   { id: "24", title: "24/7", album: "Hard To Imagine", year: 2018, url: "https://youtu.be/pEue3Tchdvc?si=7wSXZiYyTPnAEart" },
   { id: "25", title: "Sadderdaze", album: "Hard To Imagine", year: 2018, url: "https://youtu.be/ALwoKots_sg?si=EVt4_E1WKM1L7UVP" },
-  { id: "26", title: "", album: "Hard To Imagine", year: 2018, url: "https://www.youtube.com/watch?v=XTDH7gSqwiQ" },
-  { id: "27", title: "", album: "Hard To Imagine", year: 2018, url: "https://www.youtube.com/watch?v=XTDH7gSqwiQ" },
-  { id: "28", title: "", album: "Hard To Imagine", year: 2018, url: "https://www.youtube.com/watch?v=XTDH7gSqwiQ" },
-  { id: "29", title: "", album: "Hard To Imagine", year: 2018, url: "https://www.youtube.com/watch?v=XTDH7gSqwiQ" },
-  { id: "30", title: "", album: "Hard To Imagine", year: 2018, url: "https://www.youtube.com/watch?v=XTDH7gSqwiQ" },
-  { id: "31", title: "", album: "Hard To Imagine", year: 2018, url: "https://www.youtube.com/watch?v=XTDH7gSqwiQ" },
-  { id: "32", title: "", album: "Hard To Imagine", year: 2018, url: "https://www.youtube.com/watch?v=XTDH7gSqwiQ" },
+  { id: "26", title: "Reflections", album: "Hard To Imagine", year: 2018, url: "https://youtu.be/x47TgeRJtH0?si=QSvnYT1gb4riuyMI" },
+  { id: "27", title: "Void", album: "Hard To Imagine", year: 2018, url: "https://youtu.be/agdObcVqqMU?si=WwHqXzUeMdbS8QyT" },
+  { id: "28", title: "Scary Love", album: "Hard To Imagine", year: 2018, url: "https://youtu.be/4n-AbC6GK1Y?si=zEZVYZn5mNpBmfb-" },
+  { id: "29", title: "Stuck with Me", album: "Hard To Imagine", year: 2018, url: "https://youtu.be/UWBC2bv5O7U?si=JfoT8BJK1Z3WbgdR" },
+  { id: "30", title: "Softcore", album: "Hard To Imagine", year: 2018, url: "https://youtu.be/ggG9ySCChYw?si=bcIslyWP_dUcYYZs" },
+  { id: "31", title: "You Get Me So High", album: "Hard To Imagine", year: 2018, url: "https://youtu.be/jCSvOtUaI8s?si=YWoS6TrCyR7RRZJG" },
 ];
 
 export default function Hard_To_Imagine_Screen({ navigation }) {
